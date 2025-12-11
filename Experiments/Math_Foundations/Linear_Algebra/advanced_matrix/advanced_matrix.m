1d098fd1436209a4e5e1f707aeedfa31165218bb
Work_zc <1223953101@qq.com>
2025-12-11 14:05:03 +0800

    完成数学基础强化实验，更新所有文档时间戳为2025-12-11

--- Experiments/Math_Foundations/Linear_Algebra/advanced_matrix/advanced_matrix.m
+++ Experiments/Math_Foundations/Linear_Algebra/advanced_matrix/advanced_matrix.m
@@ -1,12 +1,11 @@
 % 高级矩阵运算实现
 % 包括奇异值分解（SVD）、QR分解和矩阵条件数计算
 
+% 奇异值分解（SVD）
+% 输入：矩阵A
+% 输出：U, S, V，满足A = U*S*V'
+% 其中U和V是正交矩阵，S是对角矩阵
 function [U, S, V] = advanced_svd(A)
-    % 奇异值分解（SVD）
-    % 输入：矩阵A
-    % 输出：U, S, V，满足A = U*S*V'
-    % 其中U和V是正交矩阵，S是对角矩阵
-    
     % 使用MATLAB内置的svd函数实现
     [U, S, V] = svd(A);
     
@@ -15,12 +14,11 @@ function [U, S, V] = advanced_svd(A)
     disp(diag(S));
 end
 
+% QR分解
+% 输入：矩阵A
+% 输出：Q, R，满足A = Q*R
+% 其中Q是正交矩阵，R是上三角矩阵
 function [Q, R] = advanced_qr(A)
-    % QR分解
-    % 输入：矩阵A
-    % 输出：Q, R，满足A = Q*R
-    % 其中Q是正交矩阵，R是上三角矩阵
-    
     % 使用MATLAB内置的qr函数实现
     [Q, R] = qr(A);
     
@@ -29,11 +27,10 @@ function [Q, R] = advanced_qr(A)
     disp(R);
 end
 
+% 矩阵条件数计算
+% 输入：矩阵A，范数类型p（默认2）
+% 输出：矩阵A的条件数
 function cond_num = advanced_cond(A, p)
-    % 矩阵条件数计算
-    % 输入：矩阵A，范数类型p（默认2）
-    % 输出：矩阵A的条件数
-    
     if nargin < 2
         p = 2; % 默认使用2-范数
     end
@@ -44,9 +41,8 @@ function cond_num = advanced_cond(A, p)
     disp(['矩阵条件数（', num2str(p), '-范数）：', num2str(cond_num)]);
 end
 
+% 测试高级矩阵运算功能
 function test_advanced_matrix()
-    % 测试高级矩阵运算功能
-    
     % 创建测试矩阵
     A = [1 2 3; 4 5 6; 7 8 9];
     disp('测试矩阵A：');
